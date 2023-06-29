57ee5c956323d5da4ba1bcd0931b1c5dd2017edb
Rahul <rahulchalat@yahoo.com>
2023-06-29 17:23:30 +0530

    initial

--- db.sql
+++
@@ -1,8 +0,0 @@
-CREATE table reviews(
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
-    name VARCHAR(50) NOT NULL,
-    review TEXT NOT NULL,
-    rating INT NOT NULL CHECK( rating>=1 and rating<=5 )
-
-);
